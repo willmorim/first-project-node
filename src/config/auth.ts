8c93d8bebebfc25225122b194c8079ce6c81afd5
willmorim <pdbawilliam@gmail.com>
2020-04-24 16:04:33 -0300

    fist commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a9898db0ec0102958aa38a0eefbfa74a',
+    expiresIn: '1d',
+  },
+};
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
+
+interface Request {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new AppError('Only authenticated users can change avatar.', 401);
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatartExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatartExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
