cd0ebd956771d21f5dedb3678ff59a903669691a
willmorim <pdbawilliam@gmail.com>
2020-05-27 01:41:10 -0300

    Testando criação de usuário

---
+++ src/modules/users/repositories/fakes/FakeUserRepository.ts
@@ -0,0 +1,41 @@
+import { uuid } from 'uuidv4';
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import User from '../../infra/typeorm/entities/User'; // quando é apenas 1 nivel pode usar o ../
+
+class UserRepository implements IUserRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.id === id);
+
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+
+    return findUser;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UserRepository;
---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUserService = new CreateUserService(fakeUserRepository);
+
+    const user = await createUserService.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should be able to create a new user with same email from another', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUserService = new CreateUserService(fakeUserRepository);
+
+    await createUserService.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
