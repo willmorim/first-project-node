1f77bbfc32fe938ae6830a950b03fff90cef7a8d
willmorim <pdbawilliam@gmail.com>
2020-05-26 23:44:19 -0300

    Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentServices.spec.ts
+++ src/modules/appointments/services/CreateAppointmentServices.spec.ts
@@ -1,5 +1,19 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentServices';
 
 describe('CreateAppointment', () => {
-  it('should be able t ocreate a new appointment', () => {});
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
 });
