6431df28bf100994c12ef83af3dedca950cebfd3
willmorim <pdbawilliam@gmail.com>
2020-07-05 16:11:23 -0300

    Criação do agendamento

--- src/modules/appointments/services/CreateAppointmentServices.spec.ts
+++ src/modules/appointments/services/CreateAppointmentServices.spec.ts
@@ -15,6 +15,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '123123',
       provider_id: '123123',
     });
 
@@ -27,12 +28,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointment,
+      user_id: '123123',
       provider_id: '123123',
     });
 
     await expect(
       createAppointment.execute({
         date: appointment,
+        user_id: '123123',
         provider_id: '123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
