d889e941c06b953cfdc6b54b6a14f1468518ac34
willmorim <pdbawilliam@gmail.com>
2020-07-22 23:01:09 -0300

    Correção no tsconfig e na port do redis pegando o .env

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -5,7 +5,7 @@ import { RateLimiterRedis } from 'rate-limiter-flexible';
 
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS.PORT),
+  port: Number(process.env.REDIS_PORT),
   password: process.env.REDIS_PASS || undefined,
 });
 
